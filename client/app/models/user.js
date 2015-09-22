ad250d86ace32c729de95d7996f6ae8f0a8905f7
ian jamieson <cogocreative@gmail.com>
2015-09-22 21:01:06 +0200

    sample set-up

--- client/app/models/user.js
+++ client/app/models/user.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  messages: DS.hasMany('message', {async: true}),
+  messages: DS.hasMany('message')
 });
