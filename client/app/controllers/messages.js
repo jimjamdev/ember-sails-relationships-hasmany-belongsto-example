cb7122ae79a26f9192777cac1985904a6042c43e
Ian Jamieson <cogocreative@gmail.com>
2015-09-23 09:32:43 +0200

    update to messages controller

--- client/app/controllers/messages.js
+++ client/app/controllers/messages.js
@@ -1,14 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  model: function () {
+    return this.store.createRecord('message');
+  },
+
   actions: {
     addMessage: function() {
+      var self = this;
+      var newMessage = this.store.createRecord('message');
+      var user = this.get('user');
 
-      var newMessage = this.store.createRecord('message', {
-        title: this.get('title'),
-        user: this.get('user')
-      });
-      newMessage.save().then(function() {
+      newMessage.set('user', user);
+
+      return newMessage.save().then(function() {
       }, function(error) {
         console.warn('Save Failed.', error);
       });
