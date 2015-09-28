f99e23cc2d3f67fa8e6d55e59de8705ee7ce3c6d
Ian Jamieson <cogocreative@gmail.com>
2015-09-28 13:53:56 +0200

    add belongsto string on index

--- client/app/routes/index.js
+++ client/app/routes/index.js
@@ -4,9 +4,5 @@ export default Ember.Route.extend({
   controllerName: 'messages',
   model: function() {
     return this.store.findAll('message', {sort: 'createdAt asc'});
-  },
-  /*setupController: function(controller, model){
-    controller.set('model', model);
-    controller.set('users', this.store.find('user'));
-  }*/
+  }
 });
